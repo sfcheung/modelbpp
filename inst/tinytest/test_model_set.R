8abcc0840f8543e3eac04c061ac9d44ec2e1b8b3
Shu Fai Cheung <shufai.cheung@gmail.com>
2024-01-11 23:35:59 +0800

    Add a c-method for partables and moel_set

--- inst/tinytest/test_model_set.R
+++ inst/tinytest/test_model_set.R
@@ -323,3 +323,74 @@ expect_true(all(c("user2", "user4", "user3") %in%
 plot(g)
 
 }
+
+# Test c()
+
+# Work-in-progress
+
+moda <-
+"
+x3 ~ a*x1 + b*x2
+x4 ~ a*x1
+ab := a*b
+"
+
+fita <- sem(moda, dat_path_model, fixed.x = TRUE)
+
+outa <- model_set(fita,
+                  progress = FALSE,
+                  parallel = FALSE)
+
+modb <-
+"
+x3 ~ a*x1 + b*x2
+x4 ~ a*x2
+ab := a*b
+"
+
+fitb <- sem(modb, dat_path_model, fixed.x = TRUE)
+
+outb <- model_set(fitb,
+                  progress = FALSE,
+                  parallel = FALSE)
+
+tmp <- c(outa$models, outb$models)
+expect_true(inherits(tmp, "partables"))
+expect_identical(setdiff(names(tmp),
+                         unique(c(names(outa$models),
+                                  names(outb$models)))),
+                 character(0))
+
+tmp <- c(outb, outa$models)
+expect_true(inherits(tmp, "partables"))
+expect_identical(setdiff(names(tmp),
+                         unique(c(names(outa$models),
+                                  names(outb$models)))),
+                 character(0))
+
+mod2 <-
+"
+x2 ~ x1
+x3 ~ x2
+x4 ~ x3
+"
+
+mod3 <-
+"
+x2 ~ x4
+x3 ~ x4
+x1 ~ x2 + x3
+"
+
+fit2 <- sem(mod2, dat_path_model)
+fit3 <- sem(mod3, dat_path_model)
+
+pt2 <- parameterTable(fit2)
+
+tmp <- c(outa, pt2, fit3, outb$models)
+expect_true(inherits(tmp, "partables"))
+expect_true(all(c("pt2", "fit3") %in% names(tmp)))
+
+tmp <- c(outb, user2 = pt2, user3 = fit3, outa$models)
+expect_true(inherits(tmp, "partables"))
+expect_true(all(c("user2", "user3") %in% names(tmp)))
